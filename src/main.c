65a223af34633dcf4ec76fcdfc7c5300824aa918
Steveyboi <thenewgwe@gmail.com>
2023-09-17 14:00:23 +0700

    Capitalize all the instructions

--- src/main.c
+++ src/main.c
@@ -18,14 +18,14 @@ int main(int argc, char **argv) {
 		struct nxu8_instr *instr = nxu8_decode_instr(decoder, addr);
 
 		if (instr != NULL) {
-			printf("%05Xh\t", instr->addr);
+			printf("%X:%04XH\t", instr->addr >> 16, instr->addr & 0xffff);
 			for (int x = 0; x < instr->len; x += 2) printf("%04X", nxu8_read16(decoder, addr + x));
 			if (instr->len < 4) printf("\t");
 			printf("\t%s\n", instr->assembly);
 			addr += instr->len;
 		} else {
 			uint16_t val = nxu8_read16(decoder, addr);
-			printf("%05Xh\t%04X\t\tdw %04x\n", addr, val, val);
+			printf("%X:%04XH\t%04X\t\tDW %04XH\n", addr >> 16, addr & 0xffff, val, val);
 			addr += 2;
 		}
 	}
